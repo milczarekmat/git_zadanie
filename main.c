3511c8a72aab40cfa696066960bc02c480258eaa
Mateusz Milczarek <milczary96@gmail.com>
2022-01-06 20:05:01 +0100

    Added policz function and text

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+int policz(int a, int b){
+    return a+b;
+}
+
 int main() {
     printf("Hello, World!\n");
+
+    printf("Wynik 2 + 3 = %d", policz(2, 3));
+
     return 0;
 }
